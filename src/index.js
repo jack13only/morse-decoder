d1386b5836c57c414a14856fee2be3457ab9dd93
Yauheni Shatau <jack13only@gmail.com>
2021-08-04 13:14:18 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,44 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arrWords = []
+    let count = 0;
+    let countNull = 0;
+    let outputArr = []
+    let output = ''
+
+    for (let i = 0; i < expr.length; i++) {
+        if (!(Array.isArray(arrWords[count]))) arrWords[count] = []
+        
+        if (countNull === 0 && expr[i] === '1') countNull = 1;
+        if (countNull === 1) arrWords[count].push(expr[i])
+        if (expr[i] === '*') arrWords[count] = [' ', ' ']
+        
+        if (((i + 1) % 10 === 0) && (i !== 0)) {
+            count++; 
+            countNull = 0;
+        }  
+    }
+
+    // 10 stands for dot(.), 11 stands for dash(-)
+
+    for (let i = 0; i < arrWords.length; i++) {
+        for (let j = 0; j < arrWords[i].length; j++) {
+            if (!(Array.isArray(outputArr[i]))) outputArr[i] = []
+                if (arrWords[i][j] + arrWords[i][j+1] === '11') outputArr[i].push('-')  
+                if (arrWords[i][j] + arrWords[i][j+1] === '10') outputArr[i].push('.') 
+                if (arrWords[i][j] + arrWords[i][j+1] === '  ') outputArr[i].push(' ') 
+            j++
+        }
+    }
+
+
+    for (let i = 0; i < outputArr.length; i++) {
+        output += outputArr[i] == ' ' ? ' ' : MORSE_TABLE[outputArr[i].reduce((sum, current) => sum + current, '')];
+        console.log(MORSE_TABLE['.'])
+    }
+
+    return output;   
 }
 
 module.exports = {
